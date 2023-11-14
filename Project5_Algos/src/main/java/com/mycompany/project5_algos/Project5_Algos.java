0c9e74e33fb0c9f945f96e99291aaf0ec137eff3
ceppich <ceppich@cedarville.edu>
2023-11-14 15:47:04 -0500

    uh no just getting started

---
+++ Project5_Algos/src/main/java/com/mycompany/project5_algos/Project5_Algos.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+import java.util.Scanner;
+
+package com.mycompany.project5_algos;
+
+/**
+ *
+ * @author 13016
+ */
+public class Project5_Algos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+                Scanner inputScanner = new Scanner(System.in);
+        int numVehicles = inputScanner.nextInt();
+
+    }
+}
