d488cc3b311e2aa5c5430b6aa479d336ee2b6ebe
jacobobendrado <jacobobendrado@gmail.com>
2023-11-14 17:03:33 -0500

    Almost done

--- Project5_Algos/src/main/java/com/mycompany/project5_algos/Project5_Algos.java
+++ Project5_Algos/src/main/java/com/mycompany/project5_algos/Project5_Algos.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
 package com.mycompany.project5_algos;
 import java.util.Scanner;
 
@@ -67,7 +63,19 @@ public class Project5_Algos {
         }
         
         for (int i = 0; i < numSigns; i++){
+            int u = inputScanner.nextInt();
+            int v = inputScanner.nextInt();
+            double dist = inputScanner.nextDouble();
             
+            for (int j = 0; j < numIntersections; j++) {
+                if (prev[j][u] == v) {
+                    if (cityList[j] != null) {
+                        int totalDist = (int)(best[u][j] + 0.5 - dist);
+                        
+                    }
+                }
+            }
+            System.out.println("");
         }
             
         
